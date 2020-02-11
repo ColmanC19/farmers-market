be823c5ec036a58c2722855b740159355aa6ad41
Student <example@example.com>
2020-02-11 13:24:48 -0800

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
-import './Schedule.css';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import "./Schedule.css";
+import Header from "./components/Header";
 
-import ScheduleList from './components/ScheduleList';
-
+import ScheduleList from "./components/ScheduleList";
 
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <ScheduleList/>
+      <Header />
+      <ScheduleList />
     </div>
   );
 }
